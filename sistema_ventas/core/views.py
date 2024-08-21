b637b9152130c9421890e1e6cc392684f79c0368
GaRIEN <gabrielhuaman68364gmail.com>
2024-08-21 12:04:56 -0500

    creacion backend login

--- sistema_ventas/core/views.py
+++ sistema_ventas/core/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        x_user = request.POST['txtUsuario']
+        x_pass = request.POST['txtPassword']
+        user = authenticate(request,username = x_user, password = x_pass)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+                        # Manejar errores de inicio de sesión+
+            messages.error(request, 'Credenciales inválidas. Intente nuevamente.')
+            return render(request,'core/login.html')
+    else:
+        return render(request,'core/login.html')
+    
+@login_required
+def home(request):
+    return render(request, 'core/home.html')
+
+
+def salir(request):
+    logout(request)
+    return redirect('/')

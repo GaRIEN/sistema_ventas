70dfb3a501c4db0bcc91c5ea6ca10673f8b23721
GaRIEN <gabrielhuaman683@gmail.com>
2024-08-21 01:08:05 -0500

    creacion del entorno de trabajo

--- sistema_ventas/sistema_ventas/settings.py
+++ sistema_ventas/sistema_ventas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,25 @@ WSGI_APPLICATION = 'sistema_ventas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'sicotec',
+        'USER':'remote',
+        'PASSWORD':'1234',
+        'HOST':'192.168.18.5',
+        'PORT':'3306'
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'sicotec',
+#         'USER':'root',
+#         'PASSWORD':'root',
+#         'HOST':'localhost',
+#         'PORT':'3306'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
